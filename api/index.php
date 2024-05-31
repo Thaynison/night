c8252fa0c524878ec8a9918f7a2967420843f157
Thaynison <74571396+Thaynison@users.noreply.github.com>
2024-05-30 23:56:47 -0300

    v1.0.0
    Atualização v1.0.0

---
+++ api/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>	
+	    <title>Night Roleplay | O melhor servidor de RP da América Latina</title>
+	    <link href="/styles/style.css" rel="stylesheet" type="text/css" />	
+		<link href="/styles/avisos.css" rel="stylesheet" type="text/css" />	
+		<link rel="icon" href="https://media.discordapp.net/attachments/1241831693791531048/1241832048222801961/Prancheta_4300x-8.png?ex=6652e1f8&is=66519078&hm=44536cbb0a1b20ecb80e4bf0bc7323c56c2366dbdc07587b52920a407f6f874a&=&format=webp&quality=lossless&width=810&height=810">
+	</head>
+	<body>
+		<audio autoplay loop id="music">
+			<source src="audio.mp3" type="audio/mpeg">
+		</audio>
+
+		<span>
+
+			<section id="loginDiscord" style="display: none;">
+				<div class="aviso-discord">
+					<img src="img/darps-branca.png" alt="">
+					<div class="login">
+						<h1>Bem-Vindo!</h1>
+						<h2>Faça login com discord ou registre-se</h2>
+						<button onclick="loginWithDiscord()" class="discord-buttom">
+							<a>Entrar com o Discord</a>
+						</button>
+					</div> 
+				</div>
+			</section>
+
+		</span>
+
+		<div class="top-bar">
+			<div class="left-bar">
+				<button onclick="loginWithHome()" class="barr1"><a>HOME</a></button>
+				<button onclick="loginWithRegras()" class="barr1"><a>REGRAS</a></button>
+				<button onclick="loginWithNoticias()" class="barr1"><a>NOTÍCIAS</a></button>
+				<button onclick="loginWithComoJogar()" class="barr2"><a>COMO JOGAR</a></button>
+			</div>
+			<div class="rigth-bar">
+				<button onclick="loginDiscordDiscord()"><a>LOGIN</a></button>
+			</div>
+		</div>
+
+    </body>
+	<script type="text/javascript">
+		var music = document.getElementById("music");
+		music.volume = 0.6;
+		document.addEventListener("keypress",(event) => {
+			if(event.key == "p"){
+				if(music.paused){
+					music.play();
+				} else {
+					music.pause();
+				}
+			}
+		});
+		var CLIENT_ID = '';
+		var REDIRECT_URI = '';
+		function loginDiscordDiscord() {
+		  var loginSection = document.getElementById("loginDiscord");
+		  loginSection.style.display = "block";
+		}
+        function loginWithDiscord() {
+			window.location.href = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=token&scope=identify`;
+        }
+		function loginWithRegras() {
+            window.location.href = "paginas/regras.html";
+        }
+    </script>
+</html>
---
+++ api/paginas/regras.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>	
+    <title>Night Roleplay | O melhor servidor de RP da América Latina</title>
+    <link href="../styles/regras.css" rel="stylesheet" type="text/css" />	
+    <link rel="icon" href="https://media.discordapp.net/attachments/1241831693791531048/1241832048222801961/Prancheta_4300x-8.png?ex=6652e1f8&is=66519078&hm=44536cbb0a1b20ecb80e4bf0bc7323c56c2366dbdc07587b52920a407f6f874a&=&format=webp&quality=lossless&width=810&height=810">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="sidebar">
+        <h4 class="text-center">REGRAS</h4>
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#collapseRegra1" role="button" aria-expanded="false" aria-controls="collapseRegra1">📚 INTRODUÇÃO</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#collapseRegra2" role="button" aria-expanded="false" aria-controls="collapseRegra2">✅ TERMOS E CONDIÇÕES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#collapseRegra3" role="button" aria-expanded="false" aria-controls="collapseRegra3">🌎 REGRAS GERAIS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#collapseRegra8" role="button" aria-expanded="false" aria-controls="collapseRegra8">🎭 REGRAS ROLEPLAY</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#collapseRegra4" role="button" aria-expanded="false" aria-controls="collapseRegra4">🔫 REGRAS ILEGAL</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#collapseRegra5" role="button" aria-expanded="false" aria-controls="collapseRegra5">🚔 REGRAS DA POLÍCIA</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#collapseRegra6" role="button" aria-expanded="false" aria-controls="collapseRegra6">💣 REGRAS AÇÕES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#collapseRegra7" role="button" aria-expanded="false" aria-controls="collapseRegra7">⛔ SISTEMA DE PUNIÇÕES</a>
+            </li>
+        </ul>
+    </div>
+    <div class="content">
+        <div class="collapse" id="collapseRegra1">
+            <h2 id="regra1">📚 INTRODUÇÃO</h2>
+            <br>
+            <p>Nota</p>
+            <p>Queremos te dar as boas-vindas à NIGHT ROLEPLAY! Você acaba de ingressar na melhor cidade de roleplay,
+                <br> um lugar encantador que vai te conquistar dia a dia com uma estrutura bem consolidada,
+                <br> um roleplay imersivo e uma Prefeitura presente!
+            </p>
+            <p>A proposta da NIGHT ROLEPLAY é ser uma cidade segura, divertida e acolhedora, não medindo esforços nem recursos
+                <br> para entregar sempre a melhor experiência. Nossa cidade possui regras consistentes e bem definidas que priorizam 
+                <br>o roleplay sem desmotivar os cidadãos. Este documento será seu guia de conduta.
+            </p>
+            <p>A Prefeitura estará sempre disponível para ajudar com suas dúvidas, problemas ou situações que fujam do que este
+                <br> documento descreve como correto, portanto, não hesite em nos procurar nossa equipe.
+            </p>
+            <p>Possuímos uma política de zero tolerância a comportamentos que sejam considerados como “trash rp” ou “dark rp” 
+                <br>para garantirmos que ninguém sofra nenhum tipo de assédio, preconceito ou tenham que se deparar com apologias
+                <br>a crimes reais. Repudiamos veementemente este comportamento e temos um comitê exclusivo para estes assuntos,
+                <br>garantindo que a infração seja punida com o rigor necessário.
+            </p>
+            <p>Crie sua vida, invente sua história e faça novas amizades. Seja o piloto de fuga mais famoso ou o atirador mais respeitado.
+                <br>Ingresse em um emprego legal ou se torne o chefe do tráfico. Desenvolva sua vida e faça parte da comunidade mais
+                <br>inclusiva e respeitada do FiveM.
+            </p>
+            <p>Temos a certeza de que aqui você encontrará a melhor diversão!</p>
+            <p>Boas-vindas!</p>
+            <p>Aos CEOs e Direção.</p>
+        </div>
+
+        <div class="collapse" id="collapseRegra2">
+            <h2 id="regra2">✅ TERMOS E CONDIÇÕES</h2>
+            <br>
+            <h3>ACESSO NA CIDADE</h3>
+            <p>Ao acessar nossa cidade você DECLARA QUE ESTÁ DE ACORDO com todas as regras descritas aqui, ou em quaisquer canais de nosso
+                <br>Discord, inclusive as que forem criadas posteriormente à publicação deste documento.
+            </p>
+            <p>Você declara, também, que está de acordo em ter seu personagem/voz fazendo parte de clipes, vídeos, lives ou quaisquer
+                <br>outras formas de divulgação audiovisual realizada por nossa equipe de Marketing, streamers parceiros ou streamers independentes.
+            </p>
+            <p>Qualquer conduta que esteja em desacordo com este documento, ou a critério de nossos colaboradores, poderá ser passível de punição 
+                <br>de acordo com o que está estabelecido no sistema de punições.
+            </p>
+            <br>
+            <h3>ACESSO NO DISCORD (E-MAIL)</h3>
+            <p>Ao acessar nosso Discord você deve ter ciência de que deverá seguir às regras deste documento, pois ele é uma extensão da cidade. 
+                <br>Lembre-se de que os canais possuem regras específicas, então verifique as informações fixadas sempre que houver dúvidas.
+            </p>
+            <br>
+            <h3>DENÚNCIAS</h3>
+            <p>Nossa equipe analisará as denúncias e poderá aplicar advertências, punições e/ou banimentos a seu critério de análise e você:</p>
+            <p>• Terá o direito de argumentar e apresentar provas (gravações/registros) que justifiquem ou forneçam uma visão diferente 
+                <br>do que ocorreu, antes da finalização da análise.
+            </p>
+            <p>• Deverá se portar com respeito à pessoa que abriu a denúncia e com o membro de nossa equipe responsável pela análise;</p>
+            <p>• Poderá recorrer com o colaborador que analisou o caso para revogar a denúncia/punição;</p>
+            <p>• Tem o direito de solicitar uma avaliação da Administração, quando você supor que pode ter havido algum problema na 
+                <br>interpretação ou conseguir provar que o colaborador que avaliou o caso foi parcial ou incoerente com a análise e resolução;
+            </p>
+            <br>
+            <h3>PUNIÇÕES ADVERTÊNCIAS E BANIMENTOS</h3>
+            <p>Após a análise dos casos, nossa equipe poderá aplicar, de acordo com a situação:</p>
+            <p>• Um aviso verbal;</p>
+            <p>• Uma punição* e/ou multa;</p>
+            <p>• Uma advertência, uma punição* e/ou multa;</p>
+            <p>• Um banimento temporário; uma advertência, uma punição* e/ou multa;</p>
+            <p>• Banimento.</p>
+            <p>*A punição consiste em um tempo de cadeia.</p>
+            <br>
+            <h3>DOAÇÕES E REEMBOLSO</h3>
+            <p>As doações ajudam a custear as despesas com hospedagem, equipe operacional e a manter o servidor ativo. Nós não realizamos vendas.</p>
+            <p>As doações são voluntárias, não sendo, portanto, reembolsáveis sob nenhuma hipótese.</p>
+            <p>A adição de scripts, modificações, texturas, veículos, roupas ou demais conteúdos que forem solicitados por um player será tratada 
+                <br>como uma doação quando não for exclusiva e, portanto, não serão removidas do nosso servidor por solicitação do player.
+            </p>
+            <p>Para incentivar as doações, são disponibilizados benefícios de acordo com o valor doado, como:</p>
+            <p>• Possibilidade de um salário periódico;</p>
+            <p>• Aquisição de veículos/imóveis;</p>
+            <p>• Skins ou pertences;</p>
+            <p>• Demais benefícios.</p>
+            <p>É importante ressaltar que os cidadãos, independentemente de terem doado ou não, estão sujeitos às mesmas regras e possibilidades 
+                <br>dentro da cidade, não recebendo nenhum tipo de benefício para ingressar em empregos ou terem condutas inadequadas.
+            </p>
+            <br>
+            <p style="color: red;">⚠ Os itens "permanentes" têm validade por toda a season, sendo expirados no WIPE da cidade.</p>
+            <br>
+            <h3>E-BAY E VENDA DE ITENS</h3>
+            <p>É proibido, nesta cidade, realizar o "e-bay", que é a barganha de itens de doação por dinheiro de dentro da cidade ou de itens 
+                <br>da cidade por dinheiro real. Caso seja identificado este tipo de transação, todos os players que fizeram parte deste tipo 
+                <br>de acordo terão suas contas zeradas, perdendo carros, imóveis, dinheiro, skins e itens.
+            </p>
+            <p>A venda de itens da cidade por dinheiro da cidade é permitida e as partes são OBRIGADAS  a cumprirem com o acordo firmado, 
+                <br>ou deverão devolver IMEDIATAMENTE os itens conseguidos por meio dele. Caso uma das partes não cumpra o que previa o 
+                <br>acordo e não devolva o que foi conseguido ao dono original, ela terá seu acesso na cidade restringido até que aceite 
+                <br>realizar a devolução ou cumpra o acordo.
+            </p>
+            <br>
+            <h3>WIPE</h3>
+            <p>De tempos em tempos, a cidade recebe um "wipe", este procedimento zera todo o banco de dados, apagando os personagens, 
+                <br>seus bens e o dinheiro acumulado, servindo como uma otimização, além de controlar a economia. Também é no wipe 
+                <br>que ocorre a implementação de mapas e funcionalidades, que trarão uma nova experiência para nossos cidadãos.
+            </p>
+            <p>O wipe não tem uma data específica para acontecer e dependerá de uma análise criteriosa dos Fundadores e Diretores, 
+                <br>que o programarão para o momento mais oportuno.
+            </p>
+            <br>
+            <h3>ATUALIZAÇÃO DESTE DOCUMENTO</h3>
+            <p>A NIGHT ROLEPLAY se reserva ao direito de revogar, acrescer ou modificar este documento sem prévio aviso, 
+                <br>sempre que julgar necessário. Você poderá acompanhar quaisquer mudanças em nosso Discord no canal de 
+                <br>“Anúncios” ou “Atualizações”.
+            </p>
+        </div>
+
+        <div class="collapse" id="collapseRegra3">
+            <h2 id="regra3">🌎 REGRAS GERAIS</h2>
+            <br>
+            <h3>1- Regras do Discord:</h3>
+            <p>É importante se atentar às regras dentro de nosso Discord para que seu acesso seja mantido.</p>
+            <p style="font-size: 20px;">1.1 - Ao ter seu acesso liberado na cidade, você deve permanecer em nosso Discord.</p>
+            <p style="font-size: 15px;">1.1.1- Ao sair do Discord da cidade você perde a liberação da Allowlist, juntamente com o 
+                <br>acesso na cidade, e deverá passar pelo processo de liberação novamente para reestabelecer seu acesso.
+            </p>
+            <br>
+            <p style="font-size: 20px;">1.2- Deve saber se portar com educação, não insultando, ofendendo, ameaçando ou atacando outros membros.</p>
+            <p style="font-size: 15px;">1.2.1- Caso você tenha algum tipo de comportamento inadequado poderá receber:</p>
+            <p>• Uma restrição de tempo que pode variar de minutos à dias;</p>
+            <p>• Uma advertência;</p>
+            <p>• Uma punição mais severa.</p>
+            <p>As situações serão analisadas por nossa equipe e as punições serão aplicadas de acordo com a gravidade do ocorrido.</p>
+            <br>
+            <p style="font-size: 20px;">1.3- Deve utilizar os canais de acordo com seu propósito:</p>
+            <p style="font-size: 15px;">1.3.1- No caso do uso incorreto dos canais, sua mensagem poderá ser apagada ou sua solicitação poderá não ser atendida.</p>
+            <p style="font-size: 15px;">1.3.2- A insistência em enviar mensagens inadequadas nos canais poderá resultar no artigo 1.2.1 deste documento.</p>
+            <br>
+            <p style="font-size: 20px;">1.4- Ao realizar uma denúncia ou ser denunciado, deve ter ciência de que:</p>
+            <p style="font-size: 15px;">1.4.1- Precisará fornecer todas as informações solicitadas pela equipe, para a correta análise do caso.</p>
+            <p style="font-size: 15px;">1.4.2- Não deve faltar com respeito, seja com o denunciante/denunciado ou com o responsável pela análise.</p>
+            <p style="font-size: 15px;">1.4.3- Poderá sofrer punições, caso seja constatado a sua quebra de regras nas provas da denúncia, mesmo que você seja o denunciante.</p>
+            <p style="font-size: 15px;">1.4.4- Só poderá realizar denúncias caso a ação já tenha terminado, caso contrário, ela se torna nula, mesmo que haja quebras de regras.</p>
+            <p style="font-size: 15px;">1.4.5- Deve ter ciência de que as denúncias só serão consideradas válidas caso o conteúdo denunciado tenha impactado negativamente no roleplay, 
+                <br>caso contrário ela será encerrada sem prosseguimento.
+            </p>
+            <br>
+            <p style="color: red;">❗ Ao assumir um caso, o Staff responsável se encarregará de analisar o conteúdo da denúncia, conversar com as 
+                <br>partes e definir a sentença, tendo total liberdade para aplicar as punições cabíveis para o caso, ou encerrá-lo a seu critério de 
+                <br>análise, quando constatada a ausência de motivo para sustentar uma denúncia. O player não tem nenhum tipo de autonomia para fazer 
+                <br>nenhuma exigência com relação às providências que serão tomadas para finalização do caso.
+            </p>
+            <br>
+            <p style="font-size: 20px;">1.5- Precisa utilizar um nome e sobrenome que não contenham palavras de baixo calão, de cunho sexual ou que 
+                <br>faça apologias a práticas ou situações ilícitas.
+            </p>
+            <p style="font-size: 15px;">1.5.1- Seu nome no Discord precisa ser o mesmo utilizado dentro da cidade, a troca de nome só é autorizada mediante à petição do Órgão Jurídico.</p>
+            <p style="font-size: 15px;">1.5.2- Caso seja identificado que um nome não está de acordo com os termos deste documento, o player deverá renomear seu personagem, 
+                <br>mediante solicitação da própria equipe.
+            </p>
+            <br>
+            <h3>2- Regras da cidade:</h3>
+            <p>Regras sobre o acesso e permanência na cidade.</p>
+            <p style="font-size: 20px;">2.1- Ao criar seu personagem, você:</p>
+            <p style="font-size: 15px;">2.1.1- Deverá criar suas características físicas, nome e personalidade. O nome não pode estar em desacordo com o artigo 1.5 e 
+                <br>você não poderá utilizar trajes que exponham partes íntimas em público ou para pessoas que você não tem intimidade, 
+                <br>mesmo que estejam disponíveis nas lojas de roupas.
+            </p>
+            <p style="font-size: 15px;">2.1.2- Terá livre escolha para seguir uma vida legal ou ilegal, agindo de forma condizente com o seu perfil.</p>
+            <p style="font-size: 15px;">2.1.3- Independentemente do caminho que seu personagem seguir, você não poderá utilizar este argumento para cometer <strong style="color: rgba(138, 22, 22, 0.863);">Flaming</strong> ou <strong style="color: rgba(138, 22, 22, 0.863);">Dark RP/Trash Roleplay</strong>.</p>
+            <br>
+            <p style="font-size: 20px;">2.2- Contatar a Prefeitura dentro da cidade:</p>
+            <p style="font-size: 15px;">2.2.1- As ações não poderão ser denunciadas dentro da cidade, apenas após o seu término, dentro do canal de 
+                <br>Denúncias de nosso Discord, de acordo com o que prevê o artigo 1.4 e sub-artigos.
+            </p>
+            <p style="font-size: 15px;">2.2.2- A Staff só poderá ser contatada dentro da cidade quando:</p>
+            <p>• Você se deparar com um bug, falha ou erro do GTA V ou FiveM, decorrentes do próprio jogo ou seus arquivos.</p>
+            <p>• Você presenciar uma situação de <strong style="color: rgba(138, 22, 22, 0.863);">Dark RP/Trash Roleplay:</strong> ou <strong style="color: rgba(138, 22, 22, 0.863);">Hack/Hacker:</strong>.</p>
+            <p>• Você possuir dúvidas.</p>
+            <p style="font-size: 15px;">2.2.3- Os chamados que forem realizados em desacordo com os termos 2.2.2 poderão ser ignorados</p>
+            <br>
+            <h3>3- Os chamados em geral:</h3>
+            <p>A Staff estará sempre disponível para auxiliar em dúvidas, problemas e denúncias, sempre que necessário e que o chamado 
+                <br>esteja de acordo com o que foi descrito nos artigos 1.4 e 2.2 e sub-artigos.
+            </p>
+            <p style="font-size: 20px;">3.1- Como o foco de nossa cidade é o roleplay, qualquer denúncia em que for constatado que era possível ter 
+                <br>continuado o roleplay, será desconsiderada e encerrada.
+            </p>
+            <p style="font-size: 20px;">3.2- Denúncias infundadas, com informações distorcidas ou manipuladas serão encerradas 
+                <br>e você poderá perder acesso ao sistema de denúncias por um período, a critério da Administração, além de 
+                <br>poder sofrer as sanções descritas no artigo 1.2.1.
+            </p>
+            <p style="font-size: 20px;">3.3- Só serão válidas as denúncias feitas no prazo máximo de 24 horas do ocorrido, as denúncias com mais de 
+                <br>24 horas serão declaradas nulas.
+            </p>
+            <p style="font-size: 15px;">3.3.1- Denúncias de <strong style="color: rgba(138, 22, 22, 0.863);">Dark RP/Trash Roleplay:</strong> e <strong style="color: rgba(138, 22, 22, 0.863);">Hack/Hacker:</strong> não têm prazo de expiração e poderão ser feitas a qualquer momento.</p>
+        </div>
+
+        <div class="collapse" id="collapseRegra8">
+            <h2 id="regra4">🎭 REGRAS ROLEPLAY</h2>
+            <p>Descrição detalhada da Regra 4.</p>
+        </div>
+        
+        <div class="collapse" id="collapseRegra4">
+            <h2 id="regra4">🔫 REGRAS ILEGAL</h2>
+            <p>Descrição detalhada da Regra 4.</p>
+        </div>
+
+        <div class="collapse" id="collapseRegra5">
+            <h2 id="regra5">🚔 REGRAS DA POLÍCIA</h2>
+            <p>Descrição detalhada da Regra 5.</p>
+        </div>
+
+        <div class="collapse" id="collapseRegra6">
+            <h2 id="regra6">💣 REGRAS AÇÕES</h2>
+            <p>Descrição detalhada da Regra 6.</p>
+        </div>
+
+        <div class="collapse" id="collapseRegra7">
+            <h2 id="regra7">⛔ SISTEMA DE PUNIÇÕES</h2>
+            <p>Descrição detalhada da Regra 7.</p>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
